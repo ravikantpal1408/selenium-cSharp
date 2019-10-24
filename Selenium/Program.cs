9a33a8873794b786db696475706b5f1bac3e6bdc
ravikantpal1408 <ravikantpal123@gmail.com>
2019-10-24 12:32:26 +0530

    initial commit

---
+++ Selenium/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Selenium
+{
+    class Program
+    {
+        private static IWebDriver _driverFf;
+
+        public Program(IWebDriver driverFf)
+        {
+            _driverFf = driverFf;
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            _driverFf = new FirefoxDriver(@"E:\ASP_CORE\Selenium_Work\Selenium\Selenium\Driver");
+            _driverFf.Navigate().GoToUrl("http://cryptopal1408.herokuapp.com/");
+            _driverFf.FindElement(By.ClassName("js-scroll-trigger")).Click();
+        }
+    }
+}
