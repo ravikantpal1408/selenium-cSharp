9429b4fb40824fa3e3f1f923ca675b10e503b52a
ravikantpal1408 <ravikantpal123@gmail.com>
2019-11-10 01:09:17 +0530

    just another commit

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 
 namespace Selenium
@@ -7,6 +8,7 @@ namespace Selenium
     class Program
     {
         private static IWebDriver _driverFf;
+        
 
         public Program(IWebDriver driverFf)
         {
@@ -16,10 +18,47 @@ namespace Selenium
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            _driverFf = new FirefoxDriver(@"E:\ASP_CORE\Selenium_Work\Selenium\Selenium\Driver");
-            _driverFf.Navigate().GoToUrl("http://cryptopal1408.herokuapp.com/");
-            _driverFf.FindElement(By.ClassName("js-scroll-trigger")).Click();
+            string url = "http://testing.todorvachev.com/selectors/id/";
+            string ID = "testImage";
+            string CLASSNAME = "testClass1sdfsdf";
+            
+            
+            _driverFf = new ChromeDriver(@"E:\ASP_CORE\Selenium_Work\Selenium\Selenium\Driver");
+            _driverFf.Navigate().GoToUrl(url);
+            
+            IWebElement element = _driverFf.FindElement(By.Id(ID));
+
+            try
+            {
+                if (element.Displayed)
+                {
+                    GreenMessage("Yes, I can see it");
+                    Console.WriteLine(element.Text);
+                    _driverFf.Quit();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Exception : "+ ex.ToString());
+                RedMessage("Nope, Its not there");
+                _driverFf.Quit();
+            }
+           
+            _driverFf.Quit();
+
+        }
+
+        static void RedMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+        }
+        
+        static void GreenMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(message);
         }
     }
 }
